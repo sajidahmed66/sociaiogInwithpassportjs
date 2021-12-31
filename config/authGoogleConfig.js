f6c3b76c94dd200fb191064211c936db735f6d8a
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-12-31 14:20:41 +0600

    adding googleAuth2.0 stratrgy in config file

---
+++ config/authGoogleConfig.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+const Strategy = new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+
+}, () => { });
+
+passport.use(Strategy);
